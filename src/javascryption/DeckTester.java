c4015e91a298e2b78c034528c777c35c2165dd30
aHikW <adam.hike@gmail.com>
2022-03-07 19:56:38 -0600

    added tester class and some output

---
+++ src/javascryption/DeckTester.java
@@ -0,0 +1,31 @@
+package javascryption;
+
+import java.util.ArrayList;
+
+public class DeckTester {
+
+	public static void main(String[] args) {
+		Card wolf = new Card("Wolf", 1, 2, 3);
+		Card raven = new Card("Raven", 3, 2, 1);
+		Card stout = new Card("Stout", 5, 6, 7);
+		
+		Deck deck = new Deck();
+		deck.addCard(wolf);
+		deck.addCard(raven);
+		deck.addCard(stout);
+		
+		System.out.println(deck.toString());
+		
+		System.out.println("Testing if it works with an alrady existing array list: ");
+		System.out.println("================================================");
+
+		ArrayList<Card> cardList = new ArrayList();
+		cardList.add(wolf);
+		cardList.add(raven);
+		cardList.add(stout);
+		
+		deck = new Deck(cardList);
+		System.out.println(deck.toString());
+	}
+
+}
