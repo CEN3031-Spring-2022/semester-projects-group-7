5e4738a1c7a17b9e04a9953b969dcc3a541bab05
aHikW <adam.hike@gmail.com>
2022-03-09 20:07:22 -0600

    added several functions including shuffling cards

--- src/javascryption/Deck.java
+++ src/javascryption/Deck.java
@@ -1,5 +1,6 @@
 package javascryption;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Deck {
 	private ArrayList<Card> deck; //Array List of card objects is stored here.
@@ -17,7 +18,7 @@ public class Deck {
 	 */
 	public Deck(ArrayList<Card> newDeck) {
 		this.deck = new ArrayList<Card>();
-		setDeck(newDeck);
+		addMultipleCards(newDeck);
 	}
 	
 	/**
@@ -32,9 +33,55 @@ public class Deck {
 	 * Adds any number of cards to the deck.
 	 * @param newDeck an array list of cards.
 	 */
-	public void setDeck(ArrayList<Card> newDeck) {
+	public void addMultipleCards(ArrayList<Card> newDeck) {
 		for (Card newCard: newDeck)
 			addCard(newCard);
+	}
+	
+	/**
+	 * Deletes a card.
+	 * @param cardPos Position of card you wish to delete in Array List.
+	 */
+	public void deleteCardByPosition(int cardPos) {
+		deck.remove(cardPos);
+	}
+	
+	public void shuffleCards() {
+		Random rand = new Random();
+		int randNum;
+		
+		if (this.getSize() <= 1)
+			return;
+		
+		ArrayList<Card> tempDeck = new ArrayList<Card>();
+		for (Card copy: deck)
+			tempDeck.add(copy);
+		
+		while (getSize() > 0)
+			deleteCardByPosition(0);
+		
+		while (tempDeck.size() > 0) {
+			randNum = rand.nextInt(tempDeck.size());
+			addCard(tempDeck.get(randNum));
+			tempDeck.remove(randNum);
+		}
+	}
+	
+	/**
+	 * input a position in the deck to get that card
+	 * @param cardPos integer representing the position of the card. Starts at 0.
+	 * @return Card object from given position.
+	 */
+	public Card getCardByPosition(int cardPos) {
+		return deck.get(cardPos);
+	}
+	
+	/**
+	 * Adds a new card at the front of the deck.
+	 * @param newCard new card to add.
+	 */
+	public void addCardToFront(Card newCard) {
+		deck.add(0, newCard);
 	}
 	
 	/**
@@ -58,12 +105,5 @@ public class Deck {
 		
 		return output;
 	}
-	/*
-	 * TODO: 
-	 *   - Add function that allows you to remove a card from your deck. 
-	 *       This will probably require it to go through your cards and compare them to one another.
-	 *   - Add function to shuffle deck.
-	 *   - Add function to search how many of an individual card there is.
-	 */
 	
 }
