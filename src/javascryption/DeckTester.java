82d9424df4ac29bb7ed6c70ae1f073912b819e84
aHikW <adam.hike@gmail.com>
2022-03-09 20:09:39 -0600

    Updated tests to include the new functions

--- src/javascryption/DeckTester.java
+++ src/javascryption/DeckTester.java
@@ -48,5 +48,60 @@ public class DeckTester {
 		
 		assertEquals(sut.getSize(), 3);
 	}
+	
+	@Test
+	void testDeletionOfCardsByPosition() {
+		//arrange
+		Deck sut = new Deck();
+		Card obj1 = new Card("wolf", 2, 2, 2);
+		Card obj2 = new Card("bee", 1, 1, 1);
+		sut.addCard(obj1);
+		sut.addCard(obj2);
 
+		//action - This deletes card in the first position and moves the second over.
+		sut.deleteCardByPosition(0);
+		
+		//assert - This makes sure that both toStrings are the same.
+		assertEquals(sut.getCardByPosition(0).toString(), obj2.toString());
+	}
+	
+	@Test
+	void addingCardToFrontPosition() {
+		//arrange
+		Deck sut = new Deck();
+		Card obj1 = new Card("wolf", 2, 2, 2);
+		Card obj2 = new Card("bee", 1, 1, 1);
+		Card obj3 = new Card("Cat", 0, 0, 0);
+		sut.addCard(obj1);
+		sut.addCard(obj2);
+		
+		//action - add obj3 to the front of the array.
+		sut.addCardToFront(obj3);
+		
+		//assert
+		assertEquals(sut.getCardByPosition(0).toString(), obj3.toString());
+	}
+	
+	@Test
+	void shuffleCardsKeepsAllCards() {
+		Deck sut = new Deck();
+		Card obj1 = new Card("wolf", 2, 2, 2);
+		Card obj2 = new Card("bee", 1, 1, 1);
+		Card obj3 = new Card("Cat", 0, 0, 0);
+		Card obj4 = new Card("Badger", 0, 0, 0);
+		Card obj5 = new Card("Elephant", 0, 0, 0);
+		
+		sut.addCard(obj1);
+		sut.addCard(obj2);
+		sut.addCard(obj3);
+		sut.addCard(obj4);
+		sut.addCard(obj5);
+		
+		int beforeShuffle = sut.getSize();
+		//System.out.println(sut.toString());
+		sut.shuffleCards();
+		//System.out.println("--------------\n"+sut.toString());
+		
+		assertEquals(sut.getSize(), beforeShuffle);
+	}
 }
