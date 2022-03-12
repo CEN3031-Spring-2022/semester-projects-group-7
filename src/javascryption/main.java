26cbc09a60907fe97c60d9a3a9c2575623de3e1c
Adam Williams <adam.hike@gmail.com>
2022-03-11 22:08:53 -0600

    commited by mistake

--- src/javascryption/main.java
+++ src/javascryption/main.java
@@ -1,24 +1,12 @@
 package javascryption;
 
+import static org.junit.Assert.assertEquals;
+
 public class main {
 
 	public static void main(String[] args) {
-		Deck deck = new Deck();
-		
-		Card wolf = new Card("Wolf", 1, 2, 3);
-		Card badger = new Card("Badger", 1, 2, 3);
-		Card bee = new Card("Bee", 1, 2, 3);
-		
-		deck.addCard(wolf);
-		deck.addCard(badger);
-		deck.addCard(bee);
-		
-		Player sut = new Player();
-		sut.setDeck(deck);
-		
-		sut.drawCard();
-		
-		System.out.println(sut.getPlayerDeck().getCardByPosition(0).toString());
+		System.out.println("Hello, world");
+		//we're not using this file yet. we'll be using it when developing the gui.
 	}
 
 }
