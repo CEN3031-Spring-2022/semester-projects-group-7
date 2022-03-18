ce2411089854efc388d78559e2a2817717084b3d
Adam Williams <adam.hike@gmail.com>
2022-03-18 15:35:23 -0500

    added playerTester. Forgot to commit it.

--- src/javascryption/PlayerTester.java
+++ src/javascryption/PlayerTester.java
@@ -5,13 +5,8 @@ import static org.junit.Assert.assertEquals;
 
 public class PlayerTester {
 	// TODO: check players deck to make sure it is correctly assigned
-	
 	// TODO: check players hand with at construction (shouls be empty)
-	// TODO: check players hand after inserting cards
-	
-	// TODO: check players HP at construction
-	// TODO: check players HP after decrement
-	
+
 	@Test
 	void damagePlayer() {
 		Player sut = new Player();
