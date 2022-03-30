7df2227ede935c424fb913347736be7618d19471
Adam Williams <adam.hike@gmail.com>
2022-03-29 22:23:31 -0500

    fixed comments and imports

--- src/tests/PlayerTester.java
+++ src/tests/PlayerTester.java
@@ -1,14 +1,12 @@
 package tests;
 
 import javascryption.*;
-import javascryption.Deck;
-import javascryption.Card;
 import org.junit.jupiter.api.*;
 import static org.junit.Assert.assertEquals;
 
 public class PlayerTester {
 	// TODO: check players deck to make sure it is correctly assigned
-	// TODO: check players hand with at construction (shouls be empty)
+	// TODO: check players hand with at construction (should be empty)
 
 	@Test
 	void damagePlayer() {
