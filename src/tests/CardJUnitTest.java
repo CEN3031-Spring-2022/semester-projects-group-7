65557713677338ca476567dd15f5511de64f0f97
Adam Williams <adam.hike@gmail.com>
2022-04-01 22:29:22 -0500

    card returns overkill damage and damages opposing card

--- src/tests/CardJUnitTest.java
+++ src/tests/CardJUnitTest.java
@@ -2,6 +2,9 @@ package tests;
 
 import javascryption.*;
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class CardJUnitTest {
@@ -34,6 +37,23 @@ class CardJUnitTest {
 		assertEquals(obj.getBlood(), 0);
 		assertEquals(obj.getHealth(), 1);
 		assertEquals(obj.getAttack(), 0);
+	}
+	
+	@Test
+	void cardsAttackEachotherCorrectlyAndReturnOverkillDamage() {
+		Card attacker = new Card("weasel", 2, 2, 2); //card has 2 attack
+		Card defender = new Card("wolf", 1, 1, 1); //card has 1 health
+		int overkillDamage;
+		
+		ArrayList<Card> attackerAL = new ArrayList<Card>();
+		ArrayList<Card> defenderAL = new ArrayList<Card>();
+		attackerAL.add(attacker);
+		defenderAL.add(defender);
+		
+		overkillDamage = attacker.cardAttacks(attackerAL, defenderAL, 0);
+		
+		assertEquals(-1, defender.getHealth());
+		assertEquals(1, overkillDamage);
 	}
 
 }
