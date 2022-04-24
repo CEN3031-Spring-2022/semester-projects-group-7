5f08a68ad98a41fefb344b0b8fac2f57de3d678e
Bryson Greco <bryson.greco@gmail.com>
2022-04-23 23:22:30 -0500

    fix conflict

--- src/javascryption/HandButtons.java
+++ src/javascryption/HandButtons.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package javascryption;
 
 import java.io.FileNotFoundException;
@@ -51,57 +50,3 @@ public class HandButtons{
 	}
 	
 }
-=======
-package javascryption;
-
-import java.io.FileNotFoundException;
-
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.Button;
-
-public class HandButtons{
-	
-	Button CardInHand = new Button();
-	Card handCard = new Card();
-	CardGraphicBuilder cardGraphic = new CardGraphicBuilder();
-	BoardButtons boardButtons;
-	int cardPos;
-	
-	public HandButtons(BoardButtons someBoardButtons) {
-		boardButtons = someBoardButtons;
-	}
-	
-	void addHandButton(Card card) {
-		handCard = card;
-		try {
-			cardGraphic.setPlayerCardGraphic(card, CardInHand);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		// Action Listener ///////////////////////////////////////////
-		
-    	CardInHand.setOnAction(new EventHandler<ActionEvent>() {
-    		@Override
-    		public void handle(ActionEvent event) {
-    			boardButtons.enableBoardButtons();
-    			boardButtons.setCardToCopy(handCard, cardPos);
-    		}
-    	});	
-    }
-	
-	public Button getHandButton() {
-		return CardInHand;
-	}
-	
-	public Card getHandCard() {
-		return handCard;
-	}
-	
-	public void setCardPos(int aCardPos) {
-		cardPos = aCardPos;
-	}
-	
-}
->>>>>>> refs/remotes/origin/creatingWinConditions
